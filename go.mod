15957bd340061e5f79f0263586157b6186b2f1db
takanori-matsushita <t.matsushita@arcjobz.com>
2020-08-28 16:29:01 +0900

    [add]gorm and settings

--- go.mod
+++ go.mod
@@ -6,6 +6,8 @@ require (
 	github.com/fatih/color v1.9.0 // indirect
 	github.com/fsnotify/fsnotify v1.4.9 // indirect
 	github.com/go-siris/siris v7.4.0+incompatible // indirect
+	github.com/jinzhu/gorm v1.9.16
+	github.com/joho/godotenv v1.3.0
 	github.com/labstack/echo v3.3.10+incompatible // indirect
 	github.com/labstack/echo/v4 v4.1.16
 	github.com/oxequa/interact v0.0.0-20171114182912-f8fb5795b5d7 // indirect
@@ -14,5 +16,6 @@ require (
 	github.com/sirupsen/logrus v1.6.0 // indirect
 	gopkg.in/urfave/cli.v2 v2.0.0-00010101000000-000000000000 // indirect
 )
+
 // go getでrealizeが導入できないので、以下を追記
 replace gopkg.in/urfave/cli.v2 => github.com/urfave/cli/v2 v2.2.0
--- server.go
+++ server.go
@@ -1,8 +1,13 @@
 package main
 
 import (
+	"log"
 	"net/http"
+	"os"
 
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/postgres"
+	"github.com/joho/godotenv"
 	"github.com/labstack/echo/v4"
 )
 
@@ -11,6 +16,23 @@ func hello(c echo.Context) error {
 }
 
 func main() {
+	envErr := godotenv.Load()
+	if envErr != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	dbHost := os.Getenv("DB_HOST")
+	dbPort := os.Getenv("DB_PORT")
+	dbName := os.Getenv("DB_NAME")
+	// dbUser := os.Getenv("DB_USER")
+	// dbPassword := os.Getenv("DB_PASSWORD")
+
+	db, dbErr := gorm.Open("postgres", "host="+dbHost+" "+"port="+dbPort+" "+"dbname="+dbName+" "+" "+"sslmode=disable")
+	if dbErr != nil {
+		panic("データベースへの接続に失敗しました")
+	}
+	defer db.Close()
+
 	e := echo.New()
 	e.GET("/", hello)
 	e.Logger.Fatal(e.Start(":1323"))
